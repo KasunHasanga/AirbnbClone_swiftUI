23fa390ec0153da40887e4e4c2d26a3538f1fb48
kasun Hasanga <Kasunhasanga96@gmail.com>
2023-12-11 15:36:59 +0530

    Initial Commit

---
+++ AirbnbClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirbnbClone
+//
+//  Created by kasun Hasanga on 2023-12-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
